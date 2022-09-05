2ee35d6ab1186200e477c63e107023d7be5d3a9c
Angrymouse <rykov.nikas@gmail.com>
2022-09-05 16:41:11 +0300

    build new frontend

---
+++ ui-static/public/_nuxt/default.fd79f6de.mjs
@@ -0,0 +1,1 @@
+import{x as t,o,b as r,y as n}from"./entry.c4173bb9.mjs";const s={};function a(e,c){return o(),r("div",null,[n(e.$slots,"default")])}const f=t(s,[["render",a]]);export{f as default};
---
+++ ui-static/public/_nuxt/elymus-01.f54f470b.mjs
@@ -0,0 +1,1 @@
+const e=""+new URL("elymus-01.2fbd56a3.png",import.meta.url).href;export{e as _};
