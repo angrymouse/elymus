62b0ec673a671fdd1bb617a73da42497b896a49c
Angrymouse <rykov.nikas@gmail.com>
2022-09-24 08:10:57 +0300

    Base for loader of Elymus

--- src/index.js
+++ src/index.js
@@ -1,14 +1,59 @@
 import HNSDResolver from "hnsd.js";
-const pino = require("pino");
-const fs = require("fs");
-const path = require("path");
-const { PassThrough } = require("stream");
+import * as IPFS from "ipfs-core";
+import pino from "pino";
+import fs from "fs";
+import path from "path";
+import { ClassicLevel } from "classic-level";
+import os from "os";
+import { PassThrough } from "stream";
+import defaultPreferences from "./default-preferences.json" assert { type: "json" };
+if (!fs.existsSync(path.join(os.homedir(), "elymus"))) {
+	fs.mkdirSync(path.join(os.homedir(), "elymus"));
+	fs.mkdirSync(path.join(os.homedir(), "elymus", "logs"));
+}
+// On first start we don't have folder for Elymus
 const logger = pino(
 	pino.destination(
 		path.join(
-			require("os").homedir(),
-			"elymus-logs",
+			os.homedir(),
+			"elymus",
+			"logs",
 			"elymus-fastify-log-" + Date.now() + ".txt"
 		)
 	)
 );
+async function start() {
+	global.preferences = new ClassicLevel(
+		path.join(os.homedir(), "elymus", "preferences-db"),
+		{ valueEncoding: "json", createIfMissing: true }
+	);
+	// Initialize database for preferences
+	let missingKeys = [];
+	(await global.preferences.getMany(Object.keys(defaultPreferences))).forEach(
+		(v, i) => {
+			if (v == undefined) {
+				missingKeys.push({
+					type: "put",
+					key: Object.keys(defaultPreferences)[i],
+					value: Object.values(defaultPreferences)[i],
+				});
+			}
+		}
+	);
+	await global.preferences.batch(missingKeys);
+	//Record default preferences on cold start/when preferences are deleted
+	if (fs.existsSync(path.join(os.homedir(), "elymus", "ipfs"))) {
+		fs.rmSync(path.join(os.homedir(), "elymus", "ipfs"), {
+			recursive: true,
+			force: true,
+		});
+	}
+	// Cleanup IPFS folder after previous run
+	global.ipfs = await IPFS.create({
+		repoAutoMigrate: true,
+		repo: path.join(os.homedir(), "elymus", "ipfs"),
+	});
+	global.hnsd = new HNSDResolver();
+	//Start required daemons
+}
+start();
